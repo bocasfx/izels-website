467436c3e2ce8056c5b7c9adb71b589ad804f230
rodolfo <rodolfo@gobridgit.com>
2020-04-10 08:17:14 -0400

    Colours and rasters

--- vanilla/draw.js
+++ vanilla/draw.js
@@ -1,19 +1,47 @@
-var myPath;
+paper.install(window);
+// Keep global references to both tools, so the HTML
+// links below can access them.
+var colorPink, clouds;
 
-function onMouseDown(event) {
-  myPath = new Path({
-    strokeWidth: 10,
-    strokeCap: 'round',
+window.onload = function() {
+  paper.setup('myCanvas');
+
+  var raster = new Raster('bunny');
+  raster.position = view.center;
+  raster.on('load', function() {
+    raster.size = new Size(425, 550);
   });
-  myPath.strokeColor = 'pink';
-}
 
-function onMouseDrag(event) {
-  myPath.add(event.point);
-  myPath.smooth();
-}
+  var path;
+  function onMouseDown(color) {
+    return function eventHandler(event) {
+      console.log(color)
+      path = new Path({
+        strokeWidth: 5,
+        strokeCap: 'round',
+        strokeColor: color,
+      });
+      path.add(event.point);
+      path.insertBelow(raster);
+    }
+  }
 
-function onMouseUp(event) {
-  myPath.add(event.point);
-  myPath.smooth();
+  function onMouseDrag(event) {
+    path.add(event.point);
+  }
+
+  colorPink = new Tool();
+  colorPink.onMouseDown = onMouseDown('pink');
+  colorPink.onMouseDrag = onMouseDrag;
+
+  colorPurple = new Tool();
+  colorPurple.onMouseDown = onMouseDown('purple');
+  colorPurple.onMouseDrag = onMouseDrag;
+
+  // clouds = new Tool();
+  // clouds.minDistance = 40;
+  // clouds.onMouseDown = onMouseDown('purple');
+  // clouds.onMouseDrag = function(event) {
+  //   path.arcTo(event.point);
+  // }
 }
