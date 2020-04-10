648b151ca792e1cf24c3d0760f1c8368eef4642e
rodolfo <rodolfo@gobridgit.com>
2020-04-10 16:06:26 -0400

    Document structure. Buttons.

--- vanilla/draw.js
+++ vanilla/draw.js
@@ -1,15 +1,16 @@
 paper.install(window);
 // Keep global references to both tools, so the HTML
 // links below can access them.
-var colorPink, clouds;
+var colorPink, colorPurple;
 
 window.onload = function() {
   paper.setup('myCanvas');
 
-  var raster = new Raster('bunny');
+  var raster = new Raster('sun');
   raster.position = view.center;
   raster.on('load', function() {
     raster.size = new Size(425, 550);
+    raster.opacity = 0.5;
   });
 
   var path;
@@ -30,13 +31,32 @@ window.onload = function() {
     path.add(event.point);
   }
 
+  function activateButton(color, self) {
+    var buttons = document.getElementsByClassName('colorButton');
+    console.log(buttons)
+    for (button of buttons) {
+      button.classList.remove('active');
+    };
+    var elem = document.getElementById(color);
+    if (!elem.classList.contains('active')) {
+      elem.classList.add('active');
+    }
+    self.activate();
+  }
+
   colorPink = new Tool();
   colorPink.onMouseDown = onMouseDown('pink');
   colorPink.onMouseDrag = onMouseDrag;
+  colorPink.init = function() {
+    activateButton('pink', this);
+  }
 
   colorPurple = new Tool();
   colorPurple.onMouseDown = onMouseDown('purple');
   colorPurple.onMouseDrag = onMouseDrag;
+  colorPurple.init = function() {
+    activateButton('purple', this);
+  }
 
   // clouds = new Tool();
   // clouds.minDistance = 40;
